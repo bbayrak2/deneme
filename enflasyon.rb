12fed7c7f735266cf1bfdb0e0373c5aeef2b1698
burak bayrak <burak.bayrak@bil.omu.edu.tr>
2024-05-19 19:01:25 +0300

    nsn

--- enflasyon.rb
+++ enflasyon.rb
@@ -1,6 +1,6 @@
 puts "urunun 2018 deki fiyatini giriniz          "
 a = gets.to_f
-print "urunun 2023 deki fiyatini giriniz         "
+print "urunu_n 2023 deki fiyatini giriniz         "
 b = gets.to_f
 eski = 3.70
 yeni = 28.40
