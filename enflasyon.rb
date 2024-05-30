3ef4b413f60a45f89ff908a14119d8c06b4adeba
bbayrak2 <burak.bayrak@bil.omu.edu.tr>
2024-05-30 11:52:51 +0300

    m

--- enflasyon.rb
+++
@@ -1,7 +0,0 @@
-puts "urunun 2018 deki fiyatini girin edcsxiz          "
-a = gets.to_f
-print "urunu_n 2023 deki fiyatini giriniz         "
-b = gets.to_f
-eski = 3.70
-yeni = 28.40
-print "dolar bazli enflasyon orani % #{((b /yeni)-(a /eski))/a *100}       "
